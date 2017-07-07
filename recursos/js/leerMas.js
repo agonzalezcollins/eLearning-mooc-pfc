812d7059ffc6349c94be240e2f05fadac82fdee3
Agustin Gonzalez <agonzalezcollins@frsf.utn.edu.ar>
2017-07-07 12:49:41 -0300

    Agregado lectura de pagina

--- recursos/js/leerMas.js
+++ recursos/js/leerMas.js
@@ -1,23 +1,19 @@
 // Java Script by Agustin Gonzalez
 
-function muestra() {
-    var elemento = document.getElementById("elementoAdicional");
+function muestra(numero) {
+    var elemento = document.getElementById("elementoAdicional_" + numero);
     elemento.className = "elementoVisible";
 
-    var enlaceMas = document.getElementById("enlaceLeerMas");
-	enlaceMas.className = "elementoOculto";
-	
-	 var enlaceMenos = document.getElementById("enlaceLeerMenos");
-	enlaceMenos.className = "elementoVisible";	
+    var enlace = document.getElementById("enlaceLeerMas_" + numero);
+    var funcion = "oculta(" + numero + "); return false;";
+    enlace.setAttribute("onclick", funcion);
 }
 
-function oculta() {
-    var elemento = document.getElementById("elementoAdicional");
+function oculta(numero) {
+    var elemento = document.getElementById("elementoAdicional_"+ numero);
     elemento.className = "elementoOculto";
 
-    var enlaceMas = document.getElementById("enlaceLeerMenos");
-	enlaceMas.className = "elementoOculto";
-	
-	var enlaceMenos = document.getElementById("enlaceLeerMas");
-	enlaceMenos.className = "elementoVisible";
+    var enlace = document.getElementById("enlaceLeerMas_" + numero);
+    var funcion = "muestra(" + numero + "); return false;";
+    enlace.setAttribute("onclick", funcion);
 }
